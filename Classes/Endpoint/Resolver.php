c5fcbfcefd9a6aa3c3eff22579bfa118c0076f72
Dominique Feyer <dfeyer@ttree.ch>
2017-12-12 15:43:16 +0100

    TASK: Update PHP files doc header

--- Classes/Endpoint/Resolver.php
+++ Classes/Endpoint/Resolver.php
@@ -1,15 +1,15 @@
 <?php
 namespace Flowpack\OAuth2\Client\Endpoint;
 
-/*                                                                        *
- * This script belongs to the TYPO3 Flow package "Flowpack.OAuth2.Client".*
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU General Public License, either version 3 of the   *
- * License, or (at your option) any later version.                        *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
+/*
+ * This file is part of the Flowpack.OAuth2.Client package.
+ *
+ * (c) Contributors of the Flowpack Team - flowpack.org
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Configuration\ConfigurationManager;
